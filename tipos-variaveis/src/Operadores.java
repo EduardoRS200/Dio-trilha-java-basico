b2a8b68e5a434ca729778d494c0f7c1af8d7da00
Eduardo <eduardo.r.trabalho@gmail.com>
2023-02-05 22:59:03 -0300

    Exemplos de Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,29 @@
+public class Operadores {
+
+     public static void main(String[] args) throws Exception {
+        // Tipos Operadores
+
+        String nomeCompleto = "Eduardo";
+
+        System.out.println(nomeCompleto);
+       
+        // incrementação, com ++, igual a mais 1
+        int numero = 5;
+
+        numero++;
+        System.out.println(numero -- );
+        
+        // Operador ternario, simbolos usados ?:, sempre usado expressao booleana
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ? "Verdadeiro" : "Falso";
+
+            System.out.println(resultado);
+
+        System.out.println("");
+
+    }
+}
