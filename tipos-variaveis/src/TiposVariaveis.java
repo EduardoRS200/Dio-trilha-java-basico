f2c8c7fbcc941d117eb0a96c38988f9619046bd8
Eduardo <eduardo.r.trabalho@gmail.com>
2023-02-05 22:59:26 -0300

    Exemplos de variaveis.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello,ooo World!");
+        // Tipos primitivos
+
+        String meuNome = "Eduardo Rodrigues";
+        short numeroCurto= 1;
+        int numeroNormal = 2;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 50;
+
+        System.out.println(numero);
+        // Valor que não pode ser alterado, sempre usar a palavra reservada final
+        final double VALOR_DE_PI = 3.14;
+
+    
+
     }
 }
